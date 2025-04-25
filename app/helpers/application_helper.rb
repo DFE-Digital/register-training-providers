5e4f61bfd8527e02f54bfa237cfb3bd98e69271e
Avin Hurry <avinhurry@icloud.com>
2025-04-25 08:22:42 +0100

    Add `page_data` helper to standardise page titles, headings and errors
    This will help ensure consistency across views.
    
    `page_title`  follows the regular format of the title of the current page followed by the service name.
    
    When `errors: true` the title will be prefixed with `Error: `.  It is expected that this is usually set to `errors: @the_form_object.errors.any?`
    
    `page_header` defaults to the title text but can be overridden if the two differ. The size is controllable using `header_size`.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def page_data(title:, header: nil, header_size: "l", error: false)
+    page_title = if error
+                   "Error: #{title}"
+    else
+                   title
+    end
+
+    content_for(:page_title) { page_title }
+
+    return { page_title: } if header == false
+
+    page_header = tag.h1(header || title, class: "govuk-heading-#{header_size}")
+
+    content_for(:page_header) { page_header }
+
+    { page_title:, page_header: }
+  end
 end
