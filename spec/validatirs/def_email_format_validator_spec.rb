37e97ad6c6b677a41f783b407b7c08b75c500b05
defong <470137+defong@users.noreply.github.com>
2025-05-09 17:30:26 +0100

    [0076] Added dfe email format validator

---
+++ spec/validatirs/def_email_format_validator_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe DfeEmailFormatValidator do
+  let(:record) { double('Record', email: email, errors: errors) }
+  let(:errors) { double('Errors') }
+  let(:validator) { described_class.new(record) }
+
+  describe '#validate' do
+    subject { validator.validate }
+
+    context 'when the email is valid' do
+      let(:email) { 'valid@education.gov.uk' }
+
+      it 'does not add an error to the record' do
+        allow(errors).to receive(:add)
+        subject
+        expect(errors).not_to have_received(:add)
+      end
+    end
+
+    context 'when the email is invalid' do
+      let(:email) { 'invalid@badeducation.gov.uk' }
+
+      it 'adds an error to the record' do
+        allow(errors).to receive(:add)
+        subject
+        expect(errors).to have_received(:add).with(:email, "Enter a Department for Education email address in the correct format, like name@education.gov.uk")
+      end
+    end
+  end
+end
