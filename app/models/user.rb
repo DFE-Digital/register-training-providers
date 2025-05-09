39c9accc5abff526e673193186eb8ed44d9cd938
defong <470137+defong@users.noreply.github.com>
2025-05-09 17:32:35 +0100

    [0076] Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :integer          not null, primary key
+#  dfe_sign_in_uid   :string
+#  email             :string
+#  first_name        :string
+#  last_name         :string
+#  last_signed_in_at :datetime
+#  discarded_at      :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_discarded_at  (discarded_at)
+#  index_users_on_email         (email) UNIQUE
+#
+
+class User < ApplicationRecord
+  include Discard::Model
+
+  audited
+
+  before_validation :sanitise_email
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validate do |record|
+    DfeEmailFormatValidator.new(record).validate if email.present?
+  end
+
+
+  private
+
+  def sanitise_email
+    self.email = email.gsub(/\s+/, "").downcase unless email.nil?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_12_104418) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_12_144352) do
   # These are extensions that must be enabled in order to support this database
 
   create_table "audits", force: :cascade do |t|
@@ -33,5 +33,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_12_104418) do
     t.index ["created_at"], name: "index_audits_on_created_at"
     t.index ["request_uuid"], name: "index_audits_on_request_uuid"
     t.index ["user_id", "user_type"], name: "user_index"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "dfe_sign_in_uid"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "last_signed_in_at"
+    t.datetime "discarded_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["discarded_at"], name: "index_users_on_discarded_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
