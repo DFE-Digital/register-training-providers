506239414f7b608d836b8a7eb46f98e8b426576f
defong <470137+defong@users.noreply.github.com>
2025-05-19 10:24:02 +0100

    [0076] Added view component config

---
+++ spec/support/view_components.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+
+  config.define_derived_metadata(file_path: Regexp.new("spec/components/")) do |metadata|
+    metadata[:type] = :component
+  end
+end
