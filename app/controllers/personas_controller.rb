6a5579f6bc2145316017fe5b7f57b53954052475
defong <470137+defong@users.noreply.github.com>
2025-05-20 17:11:43 +0100

    [0077] Added persona controller and view

---
+++ app/controllers/personas_controller.rb
@@ -0,0 +1,7 @@
+class PersonasController < ApplicationController
+  # skip_before_action :authenticate
+
+  def index
+    @personas = Persona.order(:email) + [Persona.non_existing_persona]
+  end
+end
