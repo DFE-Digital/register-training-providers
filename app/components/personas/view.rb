b87a5f21ea3b5202fe4d9554b8de9792685a14c7
defong <470137+defong@users.noreply.github.com>
2025-05-20 17:07:21 +0100

    [0077] Added persona view component

---
+++ app/components/personas/view.rb
@@ -0,0 +1,27 @@
+module Personas
+  class View < ViewComponent::Base
+    include ApplicationHelper
+    with_collection_parameter :persona
+    attr_reader :persona, :persona_iteration
+
+    def initialize(persona:, persona_iteration:)
+      @persona = persona
+      @persona_iteration = persona_iteration
+      super
+    end
+
+    def tag_args
+      if persona.discarded?
+        {text: "Soft deleted", colour: "grey"}
+      elsif persona.persisted?
+        {text: "Active", colour: "blue"}
+      else
+        {text: "Non existence", colour: "red"}
+      end
+    end
+
+    def not_last?
+      !persona_iteration.last?
+    end
+  end
+end
