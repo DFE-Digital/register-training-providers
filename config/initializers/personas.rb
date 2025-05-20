dc72e2ae6cc743ac58bc8a788838669cce8d15ca
defong <470137+defong@users.noreply.github.com>
2025-05-20 16:57:43 +0100

    [0077] Added personas data

---
+++ config/initializers/personas.rb
@@ -0,0 +1,10 @@
+PERSONAS = [
+  { first_name: "Anne", last_name: "Wilson", email: "test1@education.gov.uk", discarded?: false },
+  { first_name: "Mary", last_name: "Lawson", email: "test2@education.gov.uk", discarded?: false },
+  { first_name: "Colin", last_name: "Chapman", email: "test3@education.gov.uk", discarded?: false },
+  { first_name: "Marla", last_name: "Signer", email: "test4@education.gov.uk", discarded?: true },
+].freeze
+
+NON_EXISTING_PERSONA = { first_name: "Tyler", last_name: "Durden", email: "test5@education.gov.uk" }.freeze
+
+PERSONA_EMAILS = PERSONAS.map { |persona| persona[:email] }
