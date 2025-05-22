6282e6aa7a7dfd456b61e4cf4c035e72ac52cb34
defong <470137+defong@users.noreply.github.com>
2025-05-22 09:12:39 +0100

    [0076] Amended content for sign in based on the sign-in method

--- spec/features/auth/sign_in_page_spec.rb
+++ spec/features/auth/sign_in_page_spec.rb
@@ -8,24 +8,28 @@ feature "sign in page" do
   scenario "navigate to sign in", env: { sign_in_method: "persona"} do
     expect(page).to have_heading("h1", "Sign in to Register of training providers")
     expect(page).to have_title("Sign in to Register of training providers - Register of training providers - GOV.UK")
-    expect(page).to have_button("Sign in using Persona")
+    expect(page).to have_text("Sign in using a test user to access the service.")
+    expect(page).to have_button("Sign in using a test user")
   end
 
   scenario "navigate to sign in", env: { sign_in_method: nil } do
     expect(page).to have_heading("h1", "Sign in to Register of training providers")
     expect(page).to have_title("Sign in to Register of training providers - Register of training providers - GOV.UK")
+    expect(page).to have_text("Sign in using DfE Sign-in to access the service.")
     expect(page).to have_button("Sign in using DfE Sign-in")
   end
 
   scenario "navigate to sign in", env: { sign_in_method: "dfe-sign-in" } do
     expect(page).to have_heading("h1", "Sign in to Register of training providers")
     expect(page).to have_title("Sign in to Register of training providers - Register of training providers - GOV.UK")
+    expect(page).to have_text("Sign in using DfE Sign-in to access the service.")
     expect(page).to have_button("Sign in using DfE Sign-in")
   end
 
   scenario "navigate to sign in", env: { sign_in_method: "otp"} do
     expect(page).to have_heading("h1", "Sign in to Register of training providers")
     expect(page).to have_title("Sign in to Register of training providers - Register of training providers - GOV.UK")
+    expect(page).to have_text("Sign in using your email address and a one-time password.")
     expect(page).to have_button("Sign in using your email")
   end
 end
