f25072f898a343da49679fbbdfd9bfc03ad0f2c4
defong <470137+defong@users.noreply.github.com>
2025-05-22 15:20:51 +0100

    [0077] Added persona view component specs

---
+++ spec/components/personas/view_preview.rb
@@ -0,0 +1,34 @@
+module Personas
+  class ViewPreview < ViewComponent::Preview
+    def multiple_personas
+      personas = [active_user, soft_deleted_user, non_existence_user]
+      render(Personas::View.with_collection(personas))
+    end
+
+    def active_persona
+      render(Personas::View.with_collection([active_user]))
+    end
+
+    def soft_deleted_persona
+      render(Personas::View.with_collection([soft_deleted_user]))
+    end
+
+    def non_existence_persona
+      render(Personas::View.with_collection([non_existence_user]))
+    end
+
+    private
+
+    def active_user
+      @active_user ||= ::FactoryBot.create(:user)
+    end
+
+    def soft_deleted_user
+      @soft_deleted_user ||= ::FactoryBot.create(:user, :discarded)
+    end
+
+    def non_existence_user
+      @non_existence_user ||= ::FactoryBot.build(:user)
+    end
+  end
+end
