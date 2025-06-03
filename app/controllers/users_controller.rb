b0b07107b7c4a230fb5f9d323d3576f2577c2fd4
defong <470137+defong@users.noreply.github.com>
2025-06-03 21:36:24 +0100

    [0079] Added new and create related items on controller and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
   def index
     @pagy, @records = pagy(User.kept.order_by_first_then_last_name)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
+    if @user.valid?
+      # todo
+    else
+      render(:new)
+    end
+  end
 end
