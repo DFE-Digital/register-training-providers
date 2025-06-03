8c61010d366007c0f0d0f3ba42b93c1e23546930
defong <470137+defong@users.noreply.github.com>
2025-06-03 09:13:29 +0100

    [0078] Removed no longer needed specs

--- spec/support/features/matchers/service_name_header_matcher.rb
+++
@@ -1,7 +0,0 @@
-RSpec::Matchers.define :have_service_name_in_the_header do
-  match do |page|
-    page.within('.govuk-header') do
-      page.has_link?('Register of training providers', href: root_path)
-    end
-  end
-end
