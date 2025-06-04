7d4557bc956ee6f08c7903d70dbea15ffc5ed7a2
defong <470137+defong@users.noreply.github.com>
2025-06-04 17:00:00 +0100

    [0079] Added spec for new user view

---
+++ spec/support/features/matchers/back_link_matcher.rb
@@ -0,0 +1,20 @@
+RSpec::Matchers.define :have_back_link do |expected_href|
+  include CapybaraNodeHelper
+  match do |content|
+    # Wrap the content string in a Capybara node for matcher support
+    node = wrap_as_capybara_node(content)
+    node.has_link?("Back", href: expected_href)
+  end
+
+  failure_message do |content|
+    "expected that content would have a 'Back' link to #{expected_href}, but it did not.\nContent was:\n#{content}"
+  end
+
+  failure_message_when_negated do |_content|
+    "expected that content would NOT have a 'Back' link to #{expected_href}, but it did."
+  end
+
+  description do
+    "have a 'Back' link to #{expected_href}"
+  end
+end
---
+++ spec/support/features/matchers/error_summary_matcher.rb
@@ -0,0 +1,21 @@
+RSpec::Matchers.define :have_error_summary do |*expected_messages|
+  include CapybaraNodeHelper
+
+  match do |page_or_html|
+    node = wrap_as_capybara_node(page_or_html)
+    node.has_css?(".govuk-error-summary") &&
+      expected_messages.all? do |msg|
+        node.all(".govuk-error-summary__list li a").map(&:text).include?(msg)
+      end
+  end
+
+  failure_message do |page_or_html|
+    node = wrap_as_capybara_node(page_or_html)
+    actual = node.has_css?('.govuk-error-summary') ? node.all('.govuk-error-summary__list li a').map(&:text) : []
+    "expected error summary to include #{expected_messages.inspect}, but got #{actual.inspect}"
+  end
+
+  failure_message_when_negated do |_page|
+    "expected not to find error summary element with CSS selector '.govuk-error-summary', but found one"
+  end
+end
