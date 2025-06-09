8950c82ff60d593feeec3a4b060136862b8fe571
defong <470137+defong@users.noreply.github.com>
2025-06-09 18:14:12 +0100

    [0072] Added dfe sign in sso setup

---
+++ config/initializers/omniauth/dfe_sign_in.rb
@@ -0,0 +1,35 @@
+if ENV["SIGN_IN_METHOD"] == "dfe-sign-in"
+
+  OmniAuth.config.logger = Rails.logger
+
+  issuer_uri = URI.parse(ENV["DFE_SIGN_IN_ISSUER"])
+  issuer_uri_with_port = "#{issuer_uri}:#{issuer_uri.port}" if issuer_uri.present?
+
+  SETUP_PROC = lambda do |env|
+    request = Rack::Request.new(env)
+
+    redirect_uri = URI.join(request.base_url, "/auth/dfe/callback")
+
+    env["omniauth.strategy"].options.client_options = {
+      port: issuer_uri.port,
+      scheme: issuer_uri.scheme,
+      host: issuer_uri.host,
+      identifier: ENV["DFE_SIGN_IN_IDENTIFIER"],
+      secret: ENV["DFE_SIGN_IN_SECRET"],
+      redirect_uri: redirect_uri,
+    }
+  end
+
+  Rails.application.config.middleware.use(OmniAuth::Builder) do
+    provider(:openid_connect, {
+      name: :dfe,
+      discovery: true,
+      scope: %i[email profile],
+      response_type: :code,
+      path_prefix: "/auth",
+      callback_path: "/auth/dfe/callback",
+      issuer: issuer_uri_with_port,
+      setup: SETUP_PROC,
+    })
+  end
+end
