209a53b35ac1901d909cf78bf480a5a40d867fa2
defong <470137+defong@users.noreply.github.com>
2025-06-09 17:09:32 +0100

    [0072] Added support and helpers for specs

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,5 +1,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :feature) do
     config.include NavigationHelper
+    config.include DfESignInUserHelper
+    config.include AuthenticationHelper
   end
 end
