8ce619750fc403dff25f9e77949d2a8e16bf8f3c
defong <470137+defong@users.noreply.github.com>
2025-06-16 17:41:51 +0100

    [0079] Added add user flow using temporary records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,18 @@ class UsersController < ApplicationController
   include Pagy::Backend
 
   def index
+    current_user.clear_temporary(User, purpose: :check_your_answers)
     @pagy, @records = pagy(User.kept.order_by_first_then_last_name)
   end
 
   def new
-    @user = User.new
+    @user = current_user.load_temporary(User, purpose: :check_your_answers)
   end
 
   def create
     @user = User.new(params.expect(user: [:first_name, :last_name, :email]))
     if @user.valid?
+      @user.save_as_temporary!(created_by: current_user, purpose: :check_your_answers)
       redirect_to new_user_confirm_path
     else
       render(:new)
