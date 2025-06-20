30149d9afc86386b390c049ef0fe91bfdc93b7e5
defong <470137+defong@users.noreply.github.com>
2025-06-20 08:49:31 +0100

    Revert "[0079] Added demo job"
    This reverts commit 3a6b425de91a685d1df8321bd375bf74ecb0b348.

--- app/jobs/demo_job.rb
+++
@@ -1,7 +0,0 @@
-class DemoJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    Rails.logger.info "[#{Time.current}] Running scheduled DemoJob with #{args.inspect}"
-  end
-end
--- spec/jobs/demo_job_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe DemoJob, type: :job do
-  it "enqueues the job" do
-    expect {
-      DemoJob.perform_later("test")
-    }.to have_enqueued_job(DemoJob).with("test")
-  end
-end
