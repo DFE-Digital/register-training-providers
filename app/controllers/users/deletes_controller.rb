e06d09b1d5ad4174b048e93522db45de087c1886
defong <470137+defong@users.noreply.github.com>
2025-06-25 15:38:55 +0100

    [0083] Added the ability to delete a user

--- app/controllers/users/deletes_controller.rb
+++ app/controllers/users/deletes_controller.rb
@@ -2,4 +2,10 @@ class Users::DeletesController < CheckController
   def show
     @user = User.find(params[:user_id])
   end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @user.discard!
+    redirect_to(users_path, flash: { success: "Support user deleted" })
+  end
 end
---
+++ spec/features/end_to_end/users/deleting_user_spec.rb
@@ -0,0 +1,73 @@
+require "rails_helper"
+
+RSpec.feature "User management" do
+  scenario "deleting users" do
+    given_i_am_an_authenticated_user
+    and_i_have_a_user_to_delete
+    and_i_am_on_the_user_support_listing_page
+    and_i_can_see_the_page_title_support_users_with_the_count(count: 2)
+
+    and_i_click_on(current_user.name)
+    and_i_cannot_find("Delete user")
+
+    and_i_click_on("Back")
+    and_i_click_on(user_to_delete.name)
+    and_i_am_taken_to("/users/#{user_to_delete.id}")
+
+    and_i_can_see_the_page_title_for_view_support_user
+    and_i_click_on("Delete user")
+    and_i_am_taken_to("/users/#{user_to_delete.id}/delete")
+
+    and_i_can_see_the_page_title_for_confirm_you_want_to_delete_support_user
+    and_i_can_see_the_warning_text
+    when_i_click_on("Delete user")
+    then_i_see_the_success_message
+    and_i_am_taken_to("/users")
+
+    and_i_can_see_the_page_title_support_users_with_the_count(count: 1)
+    and_i_cannot_find(user_to_delete.name)
+    and_the_user_to_delete_is_deleted
+  end
+
+  def and_the_user_to_delete_is_deleted
+    expect(user_to_delete.reload).to be_discarded
+  end
+
+  def and_i_cannot_find(button_or_link)
+    expect(page).not_to have_button(button_or_link)
+    expect(page).not_to have_link(button_or_link)
+  end
+
+  def and_i_can_see_the_page_title_support_users_with_the_count(count: 1)
+    expect(page).to have_title("Support users (#{count}) - Register of training providers - GOV.UK")
+  end
+
+  def and_i_am_on_the_user_support_listing_page
+    visit "/users"
+  end
+
+  def user_to_delete
+    @user_to_delete ||= create(:user)
+  end
+
+  def and_i_can_see_the_warning_text
+    expect(page).to have_warning_text("Deleting a user is permanent – you cannot undo it.")
+  end
+  alias_method :and_i_have_a_user_to_delete, :user_to_delete
+
+  def then_i_see_the_success_message
+    expect(page).to have_notification_banner("Success", "Support user deleted")
+  end
+
+  def and_i_can_see_the_page_title_for_check_your_answers
+    expect(page).to have_title("Check your answers - Add support user - Register of training providers - GOV.UK")
+  end
+
+  def and_i_can_see_the_page_title_for_confirm_you_want_to_delete_support_user
+    expect(page).to have_title("Confirm you want to delete support user - Register of training providers - GOV.UK")
+  end
+
+  def and_i_can_see_the_page_title_for_view_support_user
+    expect(page).to have_title("View support user - Register of training providers - GOV.UK")
+  end
+end
