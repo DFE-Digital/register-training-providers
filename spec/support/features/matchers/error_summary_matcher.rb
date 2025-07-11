12af9848420ac058e578cd8caa07d5c4aa09a46b
defong <470137+defong@users.noreply.github.com>
2025-07-11 17:51:20 +0100

    [0135] Added `Is the provider accredited?` page

--- spec/support/features/matchers/error_summary_matcher.rb
+++ spec/support/features/matchers/error_summary_matcher.rb
@@ -3,16 +3,17 @@ RSpec::Matchers.define :have_error_summary do |*expected_messages|
 
   match do |page_or_html|
     node = wrap_as_capybara_node(page_or_html)
-    node.has_css?(".govuk-error-summary") &&
-      expected_messages.all? do |msg|
-        node.all(".govuk-error-summary__list li a").map(&:text).include?(msg)
-      end
+
+    return false unless node.has_css?(".govuk-error-summary")
+
+    actual_messages = node.all(".govuk-error-summary__list li a").map(&:text)
+    actual_messages == expected_messages
   end
 
   failure_message do |page_or_html|
     node = wrap_as_capybara_node(page_or_html)
     actual = node.has_css?(".govuk-error-summary") ? node.all(".govuk-error-summary__list li a").map(&:text) : []
-    "expected error summary to include #{expected_messages.inspect}, but got #{actual.inspect}"
+    "expected error summary messages to exactly match #{expected_messages.inspect}, but got #{actual.inspect}"
   end
 
   failure_message_when_negated do |_page|
