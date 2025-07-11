0885927e67ab472690ff334a5d6a1bda2c5930db
defong <470137+defong@users.noreply.github.com>
2025-07-11 17:52:01 +0100

    [0135] Added spec for the initial add provider journey

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,4 +1,26 @@
 class ProvidersController < ApplicationController
   def index
+    current_user.clear_temporary(Providers::IsTheProviderAccredited, purpose: :check_your_answers)
+  end
+
+  def new
+    @form = Providers::IsTheProviderAccredited.new
+  end
+
+  def create
+    @form = Providers::IsTheProviderAccredited.new(provider_params)
+
+    if @form.valid?
+      @form.save_as_temporary!(created_by: current_user, purpose: :check_your_answers)
+      redirect_to providers_path, flash: { success: "Provider added" }
+    else
+      render :new
+    end
+  end
+
+private
+
+  def provider_params
+    params.expect(provider: [:accreditation_status])
   end
 end
