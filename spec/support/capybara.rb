f02feb0c3a7922e70ba797342329fa7e1fb1eb67
defong <470137+defong@users.noreply.github.com>
2025-07-15 16:41:48 +0100

    [0135] Fixed screenshots filename issue

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,17 @@ Capybara.asset_host = "http://localhost:#{ENV.fetch("PORT", 1025)}"
 Capybara.save_path = "tmp/screenshots/"
 
 Capybara::Screenshot.prune_strategy = :keep_last_run
+
+timestamp = Time.zone.now.strftime("%Y-%m-%d-%H-%M-%S")
+
 Capybara::Screenshot.register_filename_prefix_formatter(:rspec) do |example|
-  example.description.tr(" ", "-").gsub(/^.*\/spec\//, "").to_s
+  description = example.full_description
+                       .downcase
+                       .gsub(/\s+/, "-") # spaces → dashes
+                       .gsub(/[\"<>|:*?\\\/\r\n]/, "") # remove invalid characters
+                       .gsub(/[^a-z0-9\-_]/, "") # strip anything not a-z, 0-9, -, _
+
+  "#{description}_#{timestamp}"
 end
+
 Capybara::Screenshot::RSpec::REPORTERS["RSpec::Core::Formatters::HtmlFormatter"] = Capybara::Screenshot::RSpec::HtmlEmbedReporter
