be4af6268158f4a65d812ed65c568c3b3a15e051
defong <470137+defong@users.noreply.github.com>
2025-07-24 07:59:15 +0100

    [0137] Added the edit page

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -12,6 +12,33 @@ class ProvidersController < ApplicationController
   end
 
   def show
-    @provider = Provider.kept.find(params[:id])
+    @provider = Provider.kept.find(id)
+  end
+
+  def edit
+    @provider = current_user.load_temporary(Provider, id: id, purpose: :edit_provider)
+  end
+
+  def update
+    @provider = current_user.load_temporary(Provider, id: id, purpose: :edit_provider)
+
+    @provider.assign_attributes(params.expect(provider: [:provider_type,
+                                                         :accreditation_status,
+                                                         :operating_name,
+                                                         :ukprn,
+                                                         :code,
+                                                         :urn,
+                                                         :legal_name]))
+
+    if @provider.valid?
+      @provider.save_as_temporary!(created_by: current_user, purpose: :edit_provider)
+      redirect_to provider_check_path(@provider)
+    else
+      render(:edit)
+    end
+  end
+
+  def id
+    params[:id].to_i
   end
 end
