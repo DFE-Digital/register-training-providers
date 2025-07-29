3e46c3d2ec578c04244a3caaba984d3de25cb241
defong <470137+defong@users.noreply.github.com>
2025-07-29 19:34:42 +0100

    [0136] Fixed code review issues for invalids

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -50,21 +50,32 @@ RSpec.describe Provider, type: :model do
     it { is_expected.to validate_presence_of(:operating_name).with_message("Enter operating name") }
     it { is_expected.to validate_presence_of(:ukprn).with_message("Enter UK provider reference number (UKPRN)") }
     it { is_expected.to allow_value("12345678").for(:ukprn) }
-    it { is_expected.not_to allow_value("1234").for(:ukprn).with_message("Enter UK provider reference number (UKPRN)") }
+    it { is_expected.not_to allow_value("1234").for(:ukprn).with_message("Enter a valid UK provider reference number (UKPRN)") }
 
     it { is_expected.to validate_presence_of(:code).with_message("Enter provider code") }
 
     it { is_expected.to allow_value("ABC").for(:code) }
     it { is_expected.to allow_value("a1B").for(:code) }
-    it { is_expected.not_to allow_value("abcdx").for(:code).with_message("Enter provider code") }
+    it { is_expected.not_to allow_value("abcdx").for(:code).with_message("Enter a valid provider code") }
 
     context "when provider_type is school or scitt" do
       [:school, :scitt].each do |provider_type|
-        let(:provider) { build(:provider, provider_type, urn: nil) }
+        let(:provider) { build(:provider, provider_type, urn:) }
 
-        it "requires URN" do
-          expect(subject.valid?).to be_falsey
-          expect(subject.errors[:urn]).to include("Enter unique reference number (URN)")
+        context "urn is set to nil" do
+          let(:urn) { nil }
+          it "requires URN" do
+            expect(subject.valid?).to be_falsey
+            expect(subject.errors[:urn]).to include("Enter unique reference number (URN)")
+          end
+        end
+
+        context "urn is set to an invalid value" do
+          let(:urn) { "invalid" }
+          it "requires valid URN" do
+            expect(subject.valid?).to be_falsey
+            expect(subject.errors[:urn]).to include("Enter a valid unique reference number (URN)")
+          end
         end
       end
     end
