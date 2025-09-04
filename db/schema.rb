1392947a992c72f7251329c4f6be83455140b2ee
Dave G <david.georgiou@icloud.com>
2025-09-04 10:22:29 +0100

    [194] Accreditation delete (#201)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_14_151518) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_04_085830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
@@ -23,6 +23,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_14_151518) do
     t.uuid "uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_accreditations_on_discarded_at"
     t.index ["end_date"], name: "index_accreditations_on_end_date"
     t.index ["number"], name: "index_accreditations_on_number"
     t.index ["provider_id"], name: "index_accreditations_on_provider_id"
