608d482d90900a688b96ccadba7953815ed820d4
David Georgiou <david.georgiou@icloud.com>
2025-09-25 15:36:05 +0100

    [210] - address model/schema/factory

---
+++ db/migrate/20250924135714_create_addresses.rb
@@ -0,0 +1,19 @@
+class CreateAddresses < ActiveRecord::Migration[8.0]
+  def change
+    create_table "addresses", id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+      t.uuid :provider_id, null: false
+      t.string :address_line_1, null: false
+      t.string :address_line_2
+      t.string :address_line_3
+      t.string :town_or_city, null: false
+      t.string :county
+      t.string :postcode, null: false
+      t.timestamps
+
+      t.index [:provider_id], name: "index_addresses_on_provider_id"
+      t.index [:postcode], name: "index_addresses_on_postcode"
+    end
+
+    add_foreign_key :addresses, :providers, column: :provider_id
+  end
+end
