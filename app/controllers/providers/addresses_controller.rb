8162ee2412eb807bdcfdac5bd72bcf1b293c07a8
David Georgiou <david.georgiou@icloud.com>
2025-09-29 16:46:04 +0100

    [240] - create an address

--- app/controllers/providers/addresses_controller.rb
+++ app/controllers/providers/addresses_controller.rb
@@ -3,4 +3,40 @@ class Providers::AddressesController < ApplicationController
     authorize provider, :show?
     @addresses = policy_scope(provider.addresses)
   end
+
+  def new
+    # Clear any existing temporary records if not coming from check page
+    current_user.clear_temporary(AddressForm, purpose: :create_address) if params[:goto] != "confirm"
+
+    @form = current_user.load_temporary(AddressForm, purpose: :create_address)
+    @form.assign_attributes(provider_id: provider.id) if @form.provider_id.blank?
+    render :new
+  end
+
+  def create
+    @form = AddressForm.new(create_address_form_params)
+
+    if @form.valid?
+      @form.save_as_temporary!(created_by: current_user, purpose: :create_address)
+      redirect_to new_provider_address_confirm_path(provider_id: provider.id)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def create_address_form_params
+    params.expect(address: [:address_line_1,
+                            :address_line_2,
+                            :address_line_3,
+                            :town_or_city,
+                            :county,
+                            :postcode,
+                            :provider_id])
+  end
+
+  def provider
+    @provider ||= Provider.find(params[:provider_id])
+  end
 end
