41c3cfca24008a3edbc3686c417dc85493a03cf8
defong <470137+defong@users.noreply.github.com>
2025-10-02 08:52:22 +0100

    [0218] Added sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,40 @@
+Sentry.init do |config|
+  config.dsn = ENV.fetch("SENTRY_DSN", nil)
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.send_default_pii = false
+
+  config.enable_logs = true
+  config.enabled_patches = [:logger]
+
+  IGNORED_PATHS = [
+    %r{^/cookies},
+    %r{^/accessibility},
+    %r{^/privacy},
+    %r{^/ping},
+    %r{^/healthcheck},
+    %r{^/sha}
+  ].freeze
+
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    op   = transaction_context[:op]
+    name = transaction_context[:name].to_s
+
+    case op
+    when /request/
+      if IGNORED_PATHS.any? { |regex| name.match?(regex) }
+        0.0
+      else
+        0.1
+      end
+    else
+      0.0
+    end
+  end
+
+  config.profiles_sample_rate = 0.1
+
+  config.release = ENV.fetch("COMMIT_SHA", nil)
+  config.environment = Rails.env
+end
