9b49afd61d22fef753524074b8fc61cebb8d69ed
Chris Gannon <cjgannon@gmail.com>
2025-10-23 10:13:25 +0100

    remove unneeded controller action

--- app/controllers/providers/contacts_controller.rb
+++ app/controllers/providers/contacts_controller.rb
@@ -4,15 +4,6 @@ class Providers::ContactsController < ApplicationController
     @contacts = policy_scope(provider.contacts)
   end
 
-  def new
-    # Clear any existing temporary records if not coming from check page
-    current_user.clear_temporary(ContactForm, purpose: :create_contact) if params[:goto] != "confirm"
-
-    @form = current_user.load_temporary(ContactForm, purpose: :create_contact)
-    @form.assign_attributes(provider_id: provider.id) if @form.provider_id.blank?
-    render :new
-  end
-
 private
 
   def provider
