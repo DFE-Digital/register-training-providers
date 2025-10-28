b3a517ad8bf14f7a66cb7d899269967eba4f6712
Chris Gannon <cjgannon@gmail.com>
2025-10-28 13:10:26 +0000

    allow null for telephone number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_28_094152) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_28_130743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
@@ -74,7 +74,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_28_094152) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email", null: false
-    t.string "telephone_number", null: false
+    t.string "telephone_number"
     t.datetime "discarded_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
