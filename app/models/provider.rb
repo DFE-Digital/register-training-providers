3b5c97b19d1071ffeaf69e5581446c0b2c809ad9
defong <470137+defong@users.noreply.github.com>
2025-11-04 20:20:30 +0000

    [0097] Amended validation precedence

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -55,8 +55,9 @@ class Provider < ApplicationRecord
 
   validates :operating_name, presence: true
   validates :ukprn, presence: true, format: { with: /\A[0-9]{8}\z/ }, length: { is: 8 }
-  validates :urn, presence: true, format: { with: /\A[0-9]{5,6}\z/ }, length: { in: 5..6 },
-                  if: :requires_urn?
+  validates :urn, presence: true, if: -> { requires_urn? }
+  validates :urn, format: { with: /\A[0-9]{5,6}\z/ }, length: { in: 5..6 },
+                  if: -> { urn.present? && requires_urn? }
   validates :code, presence: true, uniqueness: true, format: { with: /\A[A-Z0-9]{3}\z/i }, length: { is: 3 }
 
   before_save :upcase_code
