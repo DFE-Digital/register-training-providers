0372390adbed8f0456dafe9d676a4d62ac6226b0
defong <470137+defong@users.noreply.github.com>
2025-11-19 10:16:49 +0000

    [0319] Added specs for api

---
+++ spec/support/shared_examples/register_api_endpoint.rb
@@ -0,0 +1,39 @@
+RSpec.shared_examples "a register API endpoint" do |url|
+  context "with a valid authentication token" do
+    let(:token) do
+      super() || create(:authentication_token).token
+    end
+
+    before do
+      get url, headers: { Authorization: token }
+    end
+
+    it "returns status code 200" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    context "when the register_api feature flag is off", env: { feature_flag_api?: false } do
+      it "returns status code 404" do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context "with an invalid version" do
+      it "returns status code 404" do
+        invalid_version_url = url.sub(/v[.0-9]+/, "v0.0")
+        get invalid_version_url, headers: { Authorization: token }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  context "without a valid authentication token" do
+    before do
+      get url, headers: { Authorization: "Bearer fred" }
+    end
+
+    it "returns status code 401" do
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
