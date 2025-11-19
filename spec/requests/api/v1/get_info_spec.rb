ea6cc7bc2af85759594a917bba2aec5e74c05a58
defong <470137+defong@users.noreply.github.com>
2025-11-19 10:16:49 +0000

    [0319] Added specs for api

---
+++ spec/requests/api/v1/get_info_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "`GET /info` endpoint", type: :request do
+  let!(:token) { create(:authentication_token).token }
+
+  before do
+    get "/api/#{version}/info", headers: { Authorization: token }
+  end
+
+  context "using version v1" do
+    let(:version) { "v1" }
+
+    it_behaves_like "a register API endpoint", "/api/v1/info"
+
+    it "shows the requested version" do
+      expect(response.parsed_body).to eq({ "status" => "ok", "version" => { "latest" => "v1", "requested" => "v1" } })
+    end
+  end
+end
