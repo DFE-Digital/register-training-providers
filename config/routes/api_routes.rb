f45d2d7f545583602aeb37dd2d981d2eb3ff2645
defong <470137+defong@users.noreply.github.com>
2025-11-19 10:16:38 +0000

    [0319] Added routes for api

---
+++ config/routes/api_routes.rb
@@ -0,0 +1,12 @@
+module ApiRoutes
+  def self.extended(router)
+    router.instance_exec do
+      namespace :api, path: "api/:api_version", api_version: /v[.0-9]/, defaults: { format: :json } do
+        resource :info, controller: :info, only: :show, constraints: RouteConstraints::RegisterApiConstraint
+
+        # NOTE: catch all route
+        match "*url" => "base#render_not_found", via: :all
+      end
+    end
+  end
+end
