07abdaf6e273a959006dc40c13e940efad23d42f
defong <470137+defong@users.noreply.github.com>
2025-11-20 20:04:07 +0000

    [0319] Added soft delete

--- spec/models/api_client_spec.rb
+++ spec/models/api_client_spec.rb
@@ -18,7 +18,21 @@ RSpec.describe ApiClient, type: :model do
 
   describe "validations" do
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-
     it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe "before_discard callback" do
+    let!(:active_token) { create(:authentication_token, api_client:) }
+    let!(:expired_token) { create(:authentication_token, :expired, api_client:) }
+    let!(:revoked_token) { create(:authentication_token, :revoked, api_client:) }
+
+    it "revokes all active authentication tokens before discarding the client" do
+      expect {
+        api_client.discard!
+      }.to change { active_token.reload.status }.from("active").to("revoked")
+
+      expect(expired_token.reload.status).to eq("expired")
+      expect(revoked_token.reload.status).to eq("revoked")
+    end
   end
 end
