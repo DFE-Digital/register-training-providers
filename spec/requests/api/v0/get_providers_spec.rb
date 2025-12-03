caf78325d268e19ce79544a64ab3afd78790d2bc
De Fong <470137+defong@users.noreply.github.com>
2025-12-03 12:49:08 +0000

    [0325] Added changed_since to the controller and specs (#296)

--- spec/requests/api/v0/get_providers_spec.rb
+++ spec/requests/api/v0/get_providers_spec.rb
@@ -5,31 +5,41 @@ RSpec.describe "`GET /providers` endpoint", type: :request do
 
   it_behaves_like "a register API endpoint", "/api/#{version}/providers"
 
+  openapi = {
+    summary: "Get many providers",
+    tags: ["providers"],
+    description: <<~DESC
+      This endpoint can be used to retrieve providers for a given academic year.
+      This is intended to make it possible to check for new or updated providers regularly.
+    DESC
+  }
+
   context "response content" do
     let(:auth_token) { create(:authentication_token) }
     let(:token) { auth_token.token }
 
-    it "returns an array of training providers", openapi:
-    { summary: "Get many providers",
-      tags: ["providers"],
-      description: <<~DESC
-        This endpoint can be used to retrieve providers for a given academic year.
-        This is intended to make it possible to check for new or updated providers regularly.
-      DESC
-     } do
-      provider = create(:provider)
+    let(:params) do
+      { changed_since: 1.day.ago.iso8601 }
+    end
 
-      get "/api/#{version}/providers", headers: { Authorization: token }
+    let(:headers) { { Authorization: token } }
+
+    it "returns an array of training providers", openapi: do
+      create(:provider, :accredited, updated_at: 3.days.ago)
+      latest_provider = create(:provider, :accredited)
+
+      get("/api/#{version}/providers", headers:, params:)
 
       expect(response).to have_http_status(:ok)
 
+      expect(response.parsed_body[:data].count).to be(1)
       expect(response.parsed_body[:data].first).to eq(
-        { "id" => provider.id,
-          "operating_name" => provider.operating_name,
-          "provider_type" => provider.provider_type,
-          "code" => provider.code,
-          "accreditation_status" => provider.accreditation_status,
-          "updated_at" => provider.updated_at.iso8601, }
+        { "id" => latest_provider.id,
+          "operating_name" => latest_provider.operating_name,
+          "provider_type" => latest_provider.provider_type,
+          "code" => latest_provider.code,
+          "accreditation_status" => latest_provider.accreditation_status,
+          "updated_at" => latest_provider.updated_at.iso8601, }
       )
     end
   end
