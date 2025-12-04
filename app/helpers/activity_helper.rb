a31f9bb4e6e4df64506002387cc27921dce68894
David Georgiou <david.georgiou@icloud.com>
2025-12-04 16:37:39 +0000

    activity log e2e specs

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -1,6 +1,6 @@
 module ActivityHelper
   def group_audits_by_day(audits)
-    audits.group_by { |audit| audit.created_at.to_date }
+    audits.group_by { |audit| audit.created_at.in_time_zone.to_date }
   end
 end
 
