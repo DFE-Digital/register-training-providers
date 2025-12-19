2f04e7f0208ff56a16d18e7f3a5a6c32675c874a
David Georgiou <david.georgiou@icloud.com>
2025-12-19 13:41:33 +0000

    [359] - provider partnership activity log summary cards

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.1].define(version: 2025_12_18_092221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
-  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
   create_table "academic_cycles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -362,7 +361,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_18_092221) do
     t.string "purpose", default: "0", null: false
     t.string "record_type", null: false
     t.datetime "updated_at", null: false
-    t.index ["created_by", "record_type", "purpose"], name: "index_temporary_records_on_created_by_record_type_purpose", unique: true
+    t.index ["created_by", "record_type", "purpose"],
+            name: "index_temporary_records_on_created_by_record_type_purpose", unique: true
     t.index ["expires_at"], name: "index_temporary_records_on_expires_at"
   end
 
