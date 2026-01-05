af935da317cb4706b52fa2eb0a35b1571ac210fc
David Georgiou <david.georgiou@icloud.com>
2026-01-05 15:55:40 +0000

    [224] - use accessible-autocomplete over dfe unpublished version

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,21 @@
 import { initAll } from "govuk-frontend";
-import dfeAutocomplete from "dfe-autocomplete";
+import accessibleAutocomplete from "accessible-autocomplete";
 
 initAll();
-dfeAutocomplete({});
+
+// Initialize accessible-autocomplete on elements rendered by DfE::Autocomplete::View
+document
+  .querySelectorAll('[data-module="app-dfe-autocomplete"]')
+  .forEach((container) => {
+    const selectEl = container.querySelector("select");
+    if (!selectEl) return;
+
+    const defaultValue = container.getAttribute("data-default-value") || "";
+
+    accessibleAutocomplete.enhanceSelectElement({
+      selectElement: selectEl,
+      defaultValue: defaultValue,
+      minLength: 2,
+      showAllValues: true,
+    });
+  });
