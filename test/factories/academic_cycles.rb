4e2d0bb23c23a57e749476138866b79131895247
defong <470137+defong@users.noreply.github.com>
2026-01-19 17:08:42 +0000

    [0348] Added row importer for partnership

--- test/factories/academic_cycles.rb
+++ test/factories/academic_cycles.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :academic_cycle do
+    transient do
+      academic_year { Time.zone.now.year }
+    end
+
     duration do
-      current_year = Time.zone.now.year
-      Date.new(current_year, 8, 1)...Date.new(current_year + 1, 7, 31)
+      Date.new(academic_year, 8, 1)..Date.new(academic_year + 1, 7, 31)
+    end
+
+    initialize_with do
+      AcademicCycle.find_or_create_by!(duration:)
     end
   end
 end
