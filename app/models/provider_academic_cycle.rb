ae955ee1c3460f9dfdf5d397f8759bc04522affa
defong <470137+defong@users.noreply.github.com>
2026-03-13 15:26:59 +0000

    [0418] Added provider academic cycles model

---
+++ app/models/provider_academic_cycle.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: provider_academic_cycles
+#
+#  id                :uuid             not null, primary key
+#  discarded_at      :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  academic_cycle_id :uuid             not null
+#  provider_id       :uuid             not null
+#
+# Indexes
+#
+#  idx_on_provider_id_academic_cycle_id_6c9594c444      (provider_id,academic_cycle_id) UNIQUE
+#  index_provider_academic_cycles_on_academic_cycle_id  (academic_cycle_id)
+#  index_provider_academic_cycles_on_provider_id        (provider_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (academic_cycle_id => academic_cycles.id)
+#  fk_rails_...  (provider_id => providers.id)
+#
+class ProviderAcademicCycle < ApplicationRecord
+  self.implicit_order_column = :created_at
+  belongs_to :provider
+  belongs_to :academic_cycle
+  include Discard::Model
+
+  audited except: [:provider_id, :academic_cycle_id]
+end
