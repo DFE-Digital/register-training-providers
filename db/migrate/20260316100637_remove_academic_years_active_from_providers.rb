1fad9f5c88120bf4074a039972a4157682ff4e92
defong <470137+defong@users.noreply.github.com>
2026-03-16 16:02:21 +0000

    [0436] Removed academic years active from providers

---
+++ db/migrate/20260316100637_remove_academic_years_active_from_providers.rb
@@ -0,0 +1,9 @@
+class RemoveAcademicYearsActiveFromProviders < ActiveRecord::Migration[8.1]
+  def up
+    remove_column :providers, :academic_years_active
+  end
+
+  def down
+    # NOTE: not needed
+  end
+end
