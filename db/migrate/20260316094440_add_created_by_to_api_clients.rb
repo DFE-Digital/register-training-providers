c157ce621ba674adce5a1aaa1ad908ed428f515e
Chris Gannon <cjgannon@gmail.com>
2026-03-24 09:29:01 +0000

    disable db consistency checker cop for migration

--- db/migrate/20260316094440_add_created_by_to_api_clients.rb
+++ db/migrate/20260316094440_add_created_by_to_api_clients.rb
@@ -1,6 +1,8 @@
 class AddCreatedByToApiClients < ActiveRecord::Migration[8.1]
   def change
+    # rubocop:disable Rails/ColumnPresenceChecker
     add_reference :api_clients, :created_by, type: :uuid, foreign_key: { to_table: :users, on_delete: :cascade },
                                              null: false
+    # rubocop:enable Rails/ColumnPresenceChecker
   end
 end
--- db/migrate/20260316100637_remove_academic_years_active_from_providers.rb
+++ db/migrate/20260316100637_remove_academic_years_active_from_providers.rb
@@ -1,6 +1,6 @@
 class RemoveAcademicYearsActiveFromProviders < ActiveRecord::Migration[8.1]
   def up
-    # remove_column :providers, :academic_years_active
+    remove_column :providers, :academic_years_active
   end
 
   def down
