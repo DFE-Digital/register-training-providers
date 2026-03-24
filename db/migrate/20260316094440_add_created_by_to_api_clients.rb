cf9a1c08e04dfda3b96e9bdbc6de2c1159c1437f
Chris Gannon <cjgannon@gmail.com>
2026-03-24 09:32:41 +0000

    update disabled cop

--- db/migrate/20260316094440_add_created_by_to_api_clients.rb
+++ db/migrate/20260316094440_add_created_by_to_api_clients.rb
@@ -1,8 +1,8 @@
 class AddCreatedByToApiClients < ActiveRecord::Migration[8.1]
   def change
-    # rubocop:disable Rails/ColumnPresenceChecker
+    # rubocop:disable Rails/NotNullColumn
     add_reference :api_clients, :created_by, type: :uuid, foreign_key: { to_table: :users, on_delete: :cascade },
                                              null: false
-    # rubocop:enable Rails/ColumnPresenceChecker
+    # rubocop:enable Rails/NotNullColumn
   end
 end
