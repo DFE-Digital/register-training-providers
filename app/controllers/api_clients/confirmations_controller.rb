4f586043e3cc45d102e358c1c2cf1befa20c9969
Chris Gannon <cjgannon@gmail.com>
2026-04-08 12:12:31 +0100

    Update app/controllers/api_clients/confirmations_controller.rb
    Co-authored-by: De Fong <470137+defong@users.noreply.github.com>

--- app/controllers/api_clients/confirmations_controller.rb
+++ app/controllers/api_clients/confirmations_controller.rb
@@ -2,6 +2,7 @@ module ApiClients
   class ConfirmationsController < ApplicationController
     def show
       @api_client = ApiClient.find(params[:api_client_id])
+      authorize @api_client
     end
   end
 end
