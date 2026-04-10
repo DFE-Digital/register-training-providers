41f6894ca4a51ff2f222ac26db63b4a79dbb6635
defong <470137+defong@users.noreply.github.com>
2026-04-10 13:05:57 +0100

    [0437] Amended academic year helper to use details

--- spec/helpers/academic_year_helper_spec.rb
+++ spec/helpers/academic_year_helper_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe AcademicYearHelper, type: :helper do
   describe "#academic_years_row" do
     let(:academic_years) { build_list(:academic_year, 2) }
 
-    it "returns a hash with key and value" do
-      result = helper.academic_years_row(academic_years)
+    it "returns a hash with :key and :value" do
+      result = helper.academic_years_row(academic_years, false)
 
       expect(result).to include(:key, :value)
       expect(result[:key][:text]).to eq("Academic years")
     end
 
-    it "renders a bullet list of academic years" do
-      result = helper.academic_years_row(academic_years)
+    it "renders a bullet list when use_details is false" do
+      result = helper.academic_years_row(academic_years, false)
       html = result[:value][:text]
 
       expect(html).to include("govuk-list")
@@ -21,14 +21,25 @@ RSpec.describe AcademicYearHelper, type: :helper do
       expect(html).to include("<li")
     end
 
-    it "includes each academic year display text" do
+    it "wraps extra years in a <details> component when use_details is true" do
+      years = build_list(:academic_year, 5)
+
+      result = helper.academic_years_row(years, true)
+      html = result[:value][:text]
+
+      expect(html).to include("<ul").and include("<li")
+      expect(html).to include("More academic years")
+      expect(html.scan("<ul").size).to eq(2)
+    end
+
+    it "includes the formatted text for each academic year" do
       academic_years.each do |ay|
         allow(helper).to receive(:display_academic_year)
           .with(ay)
           .and_return("formatted #{ay.duration.begin.year}")
       end
 
-      result = helper.academic_years_row(academic_years)
+      result = helper.academic_years_row(academic_years, false)
       html = result[:value][:text]
 
       academic_years.each do |ay|
@@ -39,8 +50,8 @@ RSpec.describe AcademicYearHelper, type: :helper do
 
   describe "#display_academic_year" do
     let(:start_year) { 2023 }
-    let(:end_year) { 2024 }
-    let(:duration) { Date.new(start_year, 8, 1)..Date.new(end_year, 7, 31) }
+    let(:end_year)   { 2024 }
+    let(:duration)   { Date.new(start_year, 8, 1)..Date.new(end_year, 7, 31) }
 
     let(:academic_year) do
       instance_double(
@@ -53,44 +64,18 @@ RSpec.describe AcademicYearHelper, type: :helper do
     end
 
     it "formats the academic year range" do
-      result = helper.display_academic_year(academic_year)
-
-      expect(result).to eq("#{start_year} to #{end_year}")
+      expect(helper.display_academic_year(academic_year))
+        .to eq("#{start_year} to #{end_year}")
     end
 
-    context "when current" do
-      before do
-        allow(academic_year).to receive(:current?).and_return(true)
-      end
+    %i[current last next].each do |label|
+      context "when #{label}" do
+        before { allow(academic_year).to receive(:"#{label}?").and_return(true) }
 
-      it "adds current label" do
-        result = helper.display_academic_year(academic_year)
-
-        expect(result).to include("current")
-      end
-    end
-
-    context "when last" do
-      before do
-        allow(academic_year).to receive(:last?).and_return(true)
-      end
-
-      it "adds last label" do
-        result = helper.display_academic_year(academic_year)
-
-        expect(result).to include("last")
-      end
-    end
-
-    context "when next" do
-      before do
-        allow(academic_year).to receive(:next?).and_return(true)
-      end
-
-      it "adds next label" do
-        result = helper.display_academic_year(academic_year)
-
-        expect(result).to include("next")
+        it "adds the #{label} label" do
+          result = helper.display_academic_year(academic_year)
+          expect(result).to include(label.to_s)
+        end
       end
     end
 
@@ -100,31 +85,24 @@ RSpec.describe AcademicYearHelper, type: :helper do
         allow(academic_year).to receive(:next?).and_return(true)
       end
 
-      it "adds all applicable labels" do
+      it "adds all applicable labels in order" do
         result = helper.display_academic_year(academic_year)
-
         expect(result).to include("current")
         expect(result).to include("next")
+        expect(result).to match(%r{#{start_year} to #{end_year} - current - next})
       end
     end
   end
 
   describe "#academic_year_helper_text" do
     let(:start_year) { 2023 }
-    let(:end_year) { 2024 }
+    let(:end_year)   { 2024 }
+    let(:duration)   { Date.new(start_year, 8, 1)..Date.new(end_year, 7, 31) }
 
-    let(:duration) do
-      Date.new(start_year, 8, 1)..Date.new(end_year, 7, 31)
-    end
+    let(:academic_year) { instance_double("AcademicYear", duration:) }
 
-    let(:academic_year) do
-      instance_double("AcademicYear", duration:)
-    end
-
-    it "returns the correctly formatted academic year text" do
-      result = helper.academic_year_helper_text(academic_year)
-
-      expect(result).to eq(
+    it "returns the correctly formatted text" do
+      expect(helper.academic_year_helper_text(academic_year)).to eq(
         "Starts on 1 August #{start_year}, ends on 31 July #{end_year}"
       )
     end
