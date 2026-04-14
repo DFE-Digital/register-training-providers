64385143c44eefebae785d182d9c264e7201e96c
Chris Gannon <cjgannon@gmail.com>
2026-04-14 13:58:03 +0100

    [BUG] - display correct token on api client confirmation page (#455)

--- app/controllers/api_clients/confirmations_controller.rb
+++ app/controllers/api_clients/confirmations_controller.rb
@@ -1,8 +1,25 @@
 module ApiClients
   class ConfirmationsController < ApplicationController
+    before_action :redirect_if_current_active_token
+
     def show
-      @api_client = ApiClient.find(params[:api_client_id])
-      authorize @api_client
+      authorize api_client
+      @token = Api::Clients::CreateToken.call(client_name: api_client.name, created_by_email: current_user.email,
+                                              expires_at: api_client_form.expires_at.to_s).token
+    end
+
+  private
+
+    def api_client
+      @api_client ||= ApiClient.find(params[:api_client_id])
+    end
+
+    def api_client_form
+      current_user.load_temporary(ApiClientForm, purpose: :check_your_answers)
+    end
+
+    def redirect_if_current_active_token
+      redirect_to api_clients_path if api_client.current_authentication_token.present?
     end
   end
 end
--- spec/features/end_to_end/api_clients/creating_api_client_spec.rb
+++ spec/features/end_to_end/api_clients/creating_api_client_spec.rb
@@ -20,11 +20,13 @@ RSpec.feature "api_client management" do
     and_i_can_see_the_page_title_for_check_your_answers
     when_i_click_on("Save API client")
     and_i_am_taken_to("/api_clients/#{saved_api_client.id}/confirmation")
+    and_i_can_see_the_token
 
     when_i_click_on("Back to API clients")
     and_i_am_taken_to("/api_clients")
 
     and_i_can_see_the_page_title_api_clients_with_the_count(count: 1)
+    and_i_am_redirected_to_the_index_page_when_trying_to_revisit_the_confirmation_page
   end
 
   def and_i_can_see_the_page_title_api_clients_without_the_count
@@ -59,6 +61,15 @@ RSpec.feature "api_client management" do
     expect(page).to have_title("Add API client - Register of training providers - GOV.UK")
   end
 
+  def and_i_can_see_the_token
+    expect(page).to have_content(/Token: test_/)
+  end
+
+  def and_i_am_redirected_to_the_index_page_when_trying_to_revisit_the_confirmation_page
+    visit "/api_clients/#{saved_api_client.id}/confirmation"
+    and_i_am_taken_to("/api_clients")
+  end
+
   def and_i_do_not_see_error_summary
     expect(page).not_to have_error_summary
   end
@@ -71,13 +82,15 @@ RSpec.feature "api_client management" do
   end
 
   def and_i_fill_in_the_client_details_correctly
-    start_year = Date.current.year + 1
-
     page.fill_in "Client name", with: api_client.name
     within_fieldset("Expiry date") do
       fill_in "Day", with: "1"
       fill_in "Month", with: "1"
       fill_in "Year", with: start_year.to_s
     end
+  end
+
+  def start_year
+    Date.current.year + 1
   end
 end
