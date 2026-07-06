a2b75a993cbfd788b68e15414a2578e04d0a8cc7
defong <470137+defong@users.noreply.github.com>
2026-07-06 15:14:44 +0100

    [0557] Added a forbidden page content to render

---
+++ spec/features/end_to_end/forbidden_access_spec.rb
@@ -0,0 +1,80 @@
+require "rails_helper"
+
+RSpec.feature "Forbidden access" do
+  scenario "for an api user accessing providers listing" do
+    given_i_am_on_the_start_page
+    and_i_am_not_signed_in
+    and_there_is_no_sign_out_link
+    and_i_am_registered_as_an_api_user
+    and_i_have_a_dfe_sign_in_account_and_am_an_api_user
+    and_i_sign_in_via_dfe_sign_in
+
+    and_i_am_redirected_to_api_clients_page
+    and_i_click_on("Your account")
+    and_i_go_to_the_account_page
+    and_i_click_on("Register of training providers")
+    and_i_am_taken_to("/api_clients")
+
+    when_i_visit("/providers")
+    then_i_am_taken_shown_the_forbidden_page
+    when_i_click_on("Sign out")
+    then_i_logout_via_sso
+    and_i_am_taken_to("/")
+    and_i_am_not_signed_in
+  end
+
+  def given_i_am_on_the_start_page
+    visit "/"
+  end
+
+  def and_i_sign_in_via_dfe_sign_in
+    and_i_visit_the_sign_in_page
+  end
+
+  def and_i_go_to_the_users_page
+    expect(page).to have_current_path("/users")
+  end
+
+  def and_i_go_to_the_account_page
+    expect(page).to have_current_path("/account")
+  end
+
+  def then_i_logout_via_sso
+    # NOTE: 1, signs out via external sso
+    expect(current_url).to start_with("https://test-oidc.signin.education.gov.uk/session/end")
+    uri = URI.parse(current_url)
+    query = CGI.parse(uri.query)
+    expect(uri.path).to eq("/session/end")
+
+    # NOTE: 2, used the configured signout link
+    sign_out_link = "http://www.example.com/auth/dfe/sign-out"
+    expect(query["post_logout_redirect_uri"].first).to eq(sign_out_link)
+
+    # NOTE: 3, hand-off visit our sign out link afterwards
+    visit sign_out_link
+  end
+
+  def and_i_am_not_signed_in
+    expect(page).to have_link("Sign in")
+    and_there_is_no_sign_out_link
+  end
+
+  def and_i_am_redirected_to_api_clients_page
+    expect(page).to have_current_path("/api_clients")
+  end
+
+  def and_there_is_no_sign_out_link
+    expect(page).not_to have_link("Sign out")
+  end
+
+  def when_i_visit(path)
+    visit path
+  end
+
+  def then_i_am_taken_shown_the_forbidden_page
+    expect(page).to have_current_path("/providers")
+    expect(page.status_code).to eq(403)
+    expect(page).to have_content("You do not have permission to perform this action")
+    expect(page).to have_title("You do not have permission to perform this action")
+  end
+end
