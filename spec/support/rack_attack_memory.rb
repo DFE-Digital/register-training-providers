29ddaa1cb276e89e47a3efe4684d81923b931271
defong <470137+defong@users.noreply.github.com>
2026-07-17 15:11:58 +0100

    [xxxx] Fixed clean state issues

--- spec/support/rack_attack_memory.rb
+++ spec/support/rack_attack_memory.rb
@@ -1,10 +1,13 @@
 RSpec.configure do |config|
   config.around(:each, rack_attack: true) do |example|
     Rack::Attack.enabled = true
+    Rack::Attack.reset!
+
     Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
     example.run
   ensure
     Rack::Attack.cache.store.clear
+    Rack::Attack.reset!
   end
 end
