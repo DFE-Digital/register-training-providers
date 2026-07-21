18e62a75213c762d0d00c0509e11566dad8a99e9
defong <470137+defong@users.noreply.github.com>
2026-07-21 14:01:12 +0100

    [0547] Added additional loggings

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -3,6 +3,14 @@ require "rails_helper"
 RSpec.describe "Error Pages", type: :request do
   describe "GET /404" do
     it "renders the 404 error page" do
+      expect(Rails.logger).to receive(:warn).with(
+        event: "not_found",
+        user_id: nil,
+        controller: "ErrorsController",
+        action: "not_found",
+        path: "/404",
+      )
+
       get "/404"
       expect(response).to have_http_status(:not_found)
       expect(response.body).to include("Page not found")
@@ -11,6 +19,13 @@ RSpec.describe "Error Pages", type: :request do
 
   describe "GET /422" do
     it "renders the 422 error page" do
+      expect(Rails.logger).to receive(:warn).with(
+        event: "unprocessable_entity",
+        user_id: nil,
+        controller: "ErrorsController",
+        action: "unprocessable_entity",
+        path: "/422",
+      )
       get "/422"
       expect(response).to have_http_status(:unprocessable_content)
       expect(response.body).to include("Sorry, there’s a problem with the service")
@@ -19,6 +34,13 @@ RSpec.describe "Error Pages", type: :request do
 
   describe "GET /500" do
     it "renders the 500 error page" do
+      expect(Rails.logger).to receive(:warn).with(
+        event: "internal_server_error",
+        user_id: nil,
+        controller: "ErrorsController",
+        action: "internal_server_error",
+        path: "/500",
+      )
       get "/500"
       expect(response).to have_http_status(:internal_server_error)
       expect(response.body).to include("Sorry, there’s a problem with the service")
