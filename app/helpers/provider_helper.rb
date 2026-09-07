00e01b85b852f8d39e2187ac644b4a5e29cea9a1
defong <470137+defong@users.noreply.github.com>
2026-09-07 12:03:41 +0100

    [0565] Added history for provider helper

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -157,26 +157,37 @@ module ProviderHelper
 
     # Add edit actions to editable fields (skip Provider type and Accreditation status)
     editable_fields = {
-      "Operating name" => { visually_hidden_text: "operating name", href: edit_provider_path(provider) },
-      "Legal name" => { visually_hidden_text: "legal name", href: edit_provider_path(provider) },
-      "UK provider reference number (UKPRN)" => { visually_hidden_text: "UK provider reference number (UKPRN)",
-                                                  href: edit_provider_path(provider) },
-      "Unique reference number (URN)" => { visually_hidden_text: "unique reference number (URN)",
-                                           href: edit_provider_path(provider) },
-      "Provider code" => { visually_hidden_text: "provider code",
-                           href: provider_changes_path(
-                             provider_id: provider.id,
-                             field: "code"
-                           ) },
+      "Operating name" => [{ text: "Change",
+                             visually_hidden_text: "operating name",
+                             href: edit_provider_path(provider) },],
+      "Legal name" => [{ text: "Change", visually_hidden_text: "legal name", href: edit_provider_path(provider) },],
+      "UK provider reference number (UKPRN)" => [{ text: "Change",
+                                                   visually_hidden_text: "UK provider reference number (UKPRN)",
+                                                   href: edit_provider_path(provider) },],
+      "Unique reference number (URN)" => [{ text: "Change",
+                                            visually_hidden_text: "unique reference number (URN)",
+                                            href: edit_provider_path(provider) },],
+      "Provider code" => [{ text: "History",
+                            visually_hidden_text: "provider code",
+                            href: provider_changes_provider_new_path(
+                              provider_id: provider.id,
+                              field: "code"
+                            ) },
+                          { text: "Change",
+                            visually_hidden_text: "provider code",
+                            href: provider_changes_provider_new_path(
+                              provider_id: provider.id,
+                              field: "code"
+                            ) }],
     }.with_indifferent_access
 
     rows.each do |row|
       key_text = row[:key][:text]
       next unless editable_fields.key?(key_text)
 
-      row[:actions] =
-        [{ href: editable_fields[key_text][:href],
-           visually_hidden_text: editable_fields[key_text][:visually_hidden_text] }]
+      row[:actions] = editable_fields[key_text].map do |action|
+        { text: action[:text], href: action[:href], visually_hidden_text: action[:visually_hidden_text] }
+      end
     end
 
     rows
